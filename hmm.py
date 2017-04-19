00accceb005aaf711c7243b84b3db887de0e9c76
Tom Lillis <thli2089@colorado.edu>
2017-04-19 15:45:33 -0600

    made menu system

--- hmm.py
+++ hmm.py
@@ -24,7 +24,7 @@
 #               Formatting of the header comment
 #
 ######################################################################################
-import sys, getopt, operator
+import os, sys, getopt, operator
 import numpy as np
 
 
@@ -54,10 +54,15 @@ def handle_args(argv):
         print_usage()
         sys.exit(0)
 
-    if len(opts) != 4 or len(opts) != 0: # Needs two arguments to run
+    if len(opts) != 4 and len(opts) != 0: # Needs two arguments to run
+        print(len(opts))
         print('Invalid number of arguments')
         print_usage()
         sys.exit(0)
+
+    data_file = None
+    training_file = None
+    algorithm = None
 
     for opt, arg in opts:
         if opt == '-h':
@@ -77,6 +82,70 @@ def handle_args(argv):
     return data_file, training_file, algorithm
 
 ######################################################################################
+# Print Menu Options Function
+#   Prints menu options
+#
+#   Returns None
+#
+######################################################################################
+def print_menu_options():
+    print "HMM Microtubule Analysis"
+    print "1: Train"
+    print "2: Viterbi"
+    print "3: Exit"
+    return
+
+######################################################################################
+# Train HMM Function
+#   Train hmm on data file
+#
+#   Returns training probabilities
+#
+######################################################################################
+def train_hmm():
+    input_file = raw_input("Input file to use for training: ")
+    raw_input("Press enter to continue...")
+
+######################################################################################
+# Viterbi Function
+#   Runs viterbi using previously calculated probabilities
+#   Prints out accuracy calculations
+#
+#   Returns None
+#
+######################################################################################
+def viterbi():
+    input_file = raw_input("Input file to run viterbi on: ")
+    raw_input("Press enter to continue...")
+
+######################################################################################
+# Exit Program Function
+#   Exits program
+#
+######################################################################################
+def exit_program():
+    print "Exiting!"
+    exit()
+
+######################################################################################
+# Menu Actions Dictionary
+#   Options for input into main menu
+#
+######################################################################################
+menu_actions = {
+        '1' : train_hmm,
+        'train' : train_hmm,
+        't' : train_hmm,
+        '2' : viterbi,
+        'viterbi' : viterbi,
+        'v' : viterbi,
+        '3' : exit_program,
+        'exit' : exit_program,
+        'q' : exit_program,
+        'e' : exit_program,
+}
+
+######################################################################################
 # Main Function
 #   Calls functions to handle arguments, read in the sequences, and calculate alignments
 #   and then with found distance matrix caculates tree using UPGMA
@@ -88,11 +157,26 @@ def main(argv):
     # Get command line arguments
     data_file,trainging_file,algorithm  = handle_args(argv)
 
-    print("Data File:\t{0}".format(data_file))
-    print("Training File:\t{0}".format(training_file))
-    print("Algorithm:\t{0}".format(algorithm))
-    print
+    # Run using arguments
+    if data_file:
+        print("Data File:\t{0}".format(data_file))
+        print("Training File:\t{0}".format(training_file))
+        print("Algorithm:\t{0}".format(algorithm))
+        print
+        exit(0)
     
+    # Run using menu
+    os.system('clear')
+    while True:
+        print_menu_options()
+        choice = raw_input(">>  ")
+        try:
+            menu_actions[choice]()
+            os.system('clear')
+        except KeyError:
+            os.system('clear')
+            print "Invalid selection, please try again.\n"
+
     return
 
 ######################################################################################
